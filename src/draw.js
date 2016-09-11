6df9bcea555b909184a7c154b9cf1bec2f9a45a5
Nimalan Mahendran <nimalan.mahendran@gmail.com>
2016-09-10 20:20:39 -0700

    Rotation works

--- src/draw.js
+++ src/draw.js
@@ -9,22 +9,21 @@ function curvature_to_radius(r0, c0, c) {
 // coordiates given assume that the two circles and their tangent
 // points are at specific coordinates.
 
-function tangent_circle_center_internal(t, r0, r1, r2) {
+function tangent_circle_center_internal(angle, r0, r1, r2) {
   var x = r2 * (r0 + r1) / (r0 - r1);
   var y = 2*Math.sqrt(r0*r1*r2*(r0-r1-r2)) / (r0-r1);
 
-  // determine rotation from tangent point t, which is in a coordinate
-  // system where the left corner of the circle is (0,0).
-  // Snap.angle(1,1,1,0,0,0)
-  // Snap.angle(0.5,1,0.5,0,0,0)
-  // Snap.angle(1,-1,1,0,0,0)
-  var angle = Snap.angle(t[0],t[1],0,0);
-  var rotx = Snap.cos(angle)*x - Snap.sin(angle)*x;
-  var roty = Snap.sin(angle)*y + Snap.cos(angle)*y;
+  // translate to rotation coordinate system
+  x -= r0;
 
-  // left corner of the circle is still at (0,0), but the tangent
-  // circle's point has been rotated relative to the tangent point
-  // coordinates.
+  // rotate
+  var rotx = Snap.cos(angle)*x - Snap.sin(angle)*y;
+  var roty = Snap.sin(angle)*x + Snap.cos(angle)*y;
+
+  // translate back to our coordinate system
+  rotx += r0;
+  roty += r0;
+
   return [rotx, roty];
 }
 
@@ -64,22 +63,18 @@ exports.draw_gasket = function(s, cv0, cv1, cv2, cv3) {
   var ylim = cy0-cr0;
 
   var cr1 = curvature_to_radius(cr0, cv0, cv1);
-  var cx1 = xlim+cr1;
-  var cy1 = cy0;
+  var cx1 = cx0;
+  var cy1 = ylim+cr1;
   var c1 = s.circle(cx1+offsetx, cy1+offsety, cr1);
   make_circle_unfilled(c1);
 
   var cr2 = curvature_to_radius(cr0, cv0, cv2);
 
-  // var tp12 = [0,cr0];
-  // tp12[1] -= cy0;
-  var tp12 = [0,0];
-
-  var xy2 = tangent_circle_center_internal(tp12, cr0, cr1, cr2);
-  xy2[1] += +cr0;
+  var xy2 = tangent_circle_center_internal(90, cr0, cr1, cr2);
 
   var cx2 = xlim+xy2[0];
-  var cy2 = xy2[1];
+  var cy2 = ylim+xy2[1];
+
   var c2 = s.circle(cx2+offsetx, cy2+offsety, cr2);
   make_circle_unfilled(c2);
 };
